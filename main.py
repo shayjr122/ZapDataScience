26b81b5f051fdd1a5c480ff3be5df6ef64c04e89
chen <toaff100@gmail.com>
2022-06-29 20:54:37 +0300

    changes

--- main.py
+++
@@ -1,9 +0,0 @@
-from zap_crawl import ZapCrawl
-from data_process import DataProcess
-
-# zap =  ZapCrawl('data.csv')
-# zap.zap_data_crawl_all()
-data_process = DataProcess('test3.csv')
-data_process.process()
-data_process.manipulate()
-data_process.save()
